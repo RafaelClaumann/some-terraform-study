8d79214c1aa690735b5ee124bcf86dc21dd74e94
RafaelClaumann <rafael_claumann@hotmail.com>
2021-12-19 22:42:35 -0300

    melhoria sqs-sns, separando arquivos

---
+++ aws-sqs-sns/output.tf
@@ -0,0 +1,11 @@
+output "sns_arn" {
+  value = aws_sns_topic.my_sns.arn
+}
+
+output "sqs_arn" {
+  value = aws_sqs_queue.my_queue.arn
+}
+
+output "sqs_policy" {
+  value = aws_sqs_queue_policy.sqs_policy.policy
+}
---
+++ aws-sqs-sns/resources.tf
@@ -0,0 +1,49 @@
+resource "aws_sns_topic" "my_sns" {
+  name = var.topic_name
+}
+
+resource "aws_sqs_queue" "my_queue" {
+  name = var.queue_name
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.my_queue_dlq.arn,
+    maxReceiveCount     = 3
+  })
+}
+
+resource "aws_sqs_queue" "my_queue_dlq" {
+  name = "${var.queue_name}-dlq"
+}
+
+resource "aws_sqs_queue_policy" "sqs_policy" {
+  queue_url = aws_sqs_queue.my_queue.url
+
+  policy = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Id": "__default_policy_ID",
+    "Statement": [
+      {
+        "Sid": "topic-subscription",
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "sns.amazonaws.com"
+        },
+        "Action": "SQS:SendMessage",
+        "Resource": "${aws_sqs_queue.my_queue.arn}",
+        "Condition": {
+          "ArnEquals": {
+            "aws:SourceArn": "${aws_sns_topic.my_sns.arn}"
+          }
+        }
+      }
+    ]
+  }
+  POLICY
+}
+
+resource "aws_sns_topic_subscription" "sqs_sns_subscription" {
+  protocol  = "sqs"
+  topic_arn = aws_sns_topic.my_sns.arn
+  endpoint  = aws_sqs_queue.my_queue.arn
+}
