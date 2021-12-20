43d4c3eb984539bbb4c1cfad7e2f10fb2fc1a934
RafaelClaumann <rafael_claumann@hotmail.com>
2021-12-20 19:42:34 -0300

    politicas de acesso aos recursos

--- aws-sqs-sns/resources.tf
+++ aws-sqs-sns/resources.tf
@@ -4,6 +4,31 @@ resource "aws_sns_topic" "my_topic" {
   name = var.topic_name
 }
 
+resource "aws_sns_topic_policy" "default" {
+  arn = aws_sns_topic.my_topic.arn
+
+  policy = data.aws_iam_policy_document.sns_topic_policy.json
+}
+
+data "aws_iam_policy_document" "sns_topic_policy" {
+  statement {
+    actions = [
+      "SNS:Publish"
+    ]
+
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${data.aws_caller_identity.current.arn}"]
+    }
+
+    resources = [aws_sns_topic.my_topic.arn]
+
+    sid = "enable-sns-access"
+  }
+}
+
 resource "aws_sqs_queue" "my_queue" {
   name = var.queue_name
 
@@ -17,19 +42,44 @@ resource "aws_sqs_queue" "my_queue_dlq" {
   name = "${var.queue_name}-dlq"
 }
 
+resource "aws_sqs_queue_policy" "sqs_dlq_policy" {
+  queue_url = aws_sqs_queue.my_queue_dlq.url
+
+  policy = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "enable-sqs-access-dlq",
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": "${data.aws_caller_identity.current.arn}"
+        },
+        "Action": "SQS:SendMessage",
+        "Resource": "${aws_sqs_queue.my_queue_dlq.arn}",
+        "Condition": {
+          "ArnEquals": {
+            "aws:SourceArn": "${aws_sqs_queue.my_queue.arn}"
+          }
+        }
+      }
+    ]
+  }
+  POLICY
+}
+
 resource "aws_sqs_queue_policy" "sqs_policy" {
   queue_url = aws_sqs_queue.my_queue.url
 
   policy = <<POLICY
   {
     "Version": "2012-10-17",
-    "Id": "__default_policy_ID",
     "Statement": [
       {
-        "Sid": "enable-iam",
+        "Sid": "enable-sqs-access",
         "Effect": "Allow",
         "Principal": {
-          "AWS": "${data.aws_caller_identity.current.account_id}"
+          "Service": "sqs.amazonaws.com"
         },
         "Action": ["SQS:SendMessage", "SQS:ReceiveMessage"],
         "Resource": "${aws_sqs_queue.my_queue.arn}"
